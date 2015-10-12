6f1f8c49a19ced00b0e4abdda551691f973d35f9
Peter Alserda <peteralserda@hotmail.com>
2015-10-12 23:30:57 +0200

    geolocations added

--- app/models/stand.rb
+++ app/models/stand.rb
@@ -1,3 +1,9 @@
 class Stand < ActiveRecord::Base
   belongs_to :organisation
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    "#{address}, #{postalcode}"
+  end
 end
